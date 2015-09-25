3a96a1390fc39cae581b2fd92673fc1e093f40d7
SeanCoder <seandscott@me.com>
2015-09-25 16:47:16 -0700

    Fix error in karma by loading modules first

--- app/templates/skeleton/karma.conf.js
+++ app/templates/skeleton/karma.conf.js
@@ -36,7 +36,8 @@ module.exports = function (config) {
             'bower_components/angular-touch/angular-touch.js',
             'bower_components/angular-mocks/angular-mocks.js',
             // endbower
-            "app/scripts/**/*.js",
+            "app/scripts/**/*.module.js",
+            "app/scripts/**/!(*.module).js",
             "app/scripts/**/*.html"
             // "test/**/*.js"
         ],
